ffe950d68259351154c150149bee25edbc8be258
Pedro Silva <silva.mfpedro@gmail.com>
2019-02-21 11:23:13 +0000

    Changes in player_silvamfpedro.cpp file

--- player_silvamfpedro/src/player_silvamfpedro.cpp
+++ player_silvamfpedro/src/player_silvamfpedro.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include <ros/ros.h>
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "player_psilva");
-    ros::NodeHandle n;
+class Player{
+public:
+    //properties
+    std::string player_name;
+    std::string team_name = "";
 
-    for(int i = 0; i < 10; i++){
-        std::cout << i << std::endl;
-        
+    Player(std::string player_name_in){ //constructor
+        player_name = player_name_in;
+        team_name = "";
     }
+
+    void setTeamName(std::string team_name_in){
+        if(team_name_in == "red" || team_name_in == "blue" || team_name_in == "green"){
+            team_name = team_name_in;
+        }
+        else{
+            std::cout << "Invalid team name" << team_name_in << std::endl;
+            team_name = "";
+        }
+    }
+private:
+
+};
+
+int main(int argc, char** argv){
+    //ros::init(argc, argv, "player_psilva");
+    //ros::NodeHandle n;
+
+    //for(int i = 0; i < 10; i++){
+    //    std::cout << i << std::endl;
+    //}
     std::cout << "Hello World" << std::endl;
+    Player player("psilva");
+    std::cout << "Hello World from psilva" << std::endl;
+    player.setTeamName("blue");
+    std::cout << "Hellow World from psilva of team blue" << std::endl;
     return 1;
 }
